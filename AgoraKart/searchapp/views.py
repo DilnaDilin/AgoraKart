918d68e3975bd5eb940e7384188289da449ec9ed
DilnaDilin <124145633+DilnaDilin@users.noreply.github.com>
2024-02-01 13:38:11 +0400

    Update views.py

--- AgoraKart/searchapp/views.py
+++ AgoraKart/searchapp/views.py
@@ -8,7 +8,8 @@ def searchProd(request):
     query=None
     if request.method == 'GET' and 'q' in request.GET:
         query=request.GET.get('q')
-        products=Product.objects.all().filter(Q(name__icontains=query)| Q(description__icontains=query))
+        if query:
+            products=Product.objects.all().filter(Q(name__icontains=query)| Q(description__icontains=query))
     print(query)
     print(products)
     return render(request,'search.html',{'query':query, 'products':products})
